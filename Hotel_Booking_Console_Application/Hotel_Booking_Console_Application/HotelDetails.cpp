3862c34ac7a5fb18ac15268b5c9087aa6b034b0d
Henrik Jangefelt <henke9105@hotmail.com>
2020-12-04 02:04:05 +0100

    Hotel Details class

---
+++ Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/HotelDetails.cpp
@@ -0,0 +1,31 @@
+#include "HotelDetails.h"
+
+HotelDetails& HotelDetails::setHotelName(const std::string& name)
+{
+	m_hotelName = name;
+	return *this;
+}
+
+HotelDetails& HotelDetails::setContactDetails(ContactDetails& details)
+{
+	m_contactDetails = std::make_unique<ContactDetails>(details);
+	return *this;
+}
+
+HotelDetails& HotelDetails::addAmenity(const Amenity& amenity)
+{
+	m_amenities.push_back(std::move(amenity));
+	return *this;
+}
+
+HotelDetails& HotelDetails::setDistanceToOcean(int meters)
+{
+	m_metersToOcean = meters;
+	return *this;
+}
+
+HotelDetails& HotelDetails::setStarRating(int stars)
+{
+	m_starRating = stars;
+	return *this;
+}
---
+++ Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/HotelDetails.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Amenity.h"
+#include "ContactDetails.h"
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+class HotelDetails
+{
+public:
+	using amenityList_t = std::vector<Amenity>;
+	using contactDetails_t = std::unique_ptr<ContactDetails>;
+
+public:
+
+	HotelDetails& setHotelName(const std::string& name);
+	HotelDetails& setContactDetails(ContactDetails& details);
+	HotelDetails& addAmenity(const Amenity& amenity);
+	HotelDetails& setDistanceToOcean(int meters);
+	HotelDetails& setStarRating(int stars);
+
+	friend std::ostream& operator<< (std::ostream& out, const HotelDetails& details);
+
+private:
+	std::string		   m_hotelName;
+	contactDetails_t   m_contactDetails;
+	amenityList_t	   m_amenities;
+	std::optional<int> m_metersToOcean;
+	int				   m_starRating;
+};
+
