1ef398698f2b0156511ce7eb9acc460123d3f3b6
Henrik Jangefelt <henke9105@hotmail.com>
2020-12-04 02:07:01 +0100

    Contact Details class

---
+++ Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/ContactDetails.cpp
@@ -0,0 +1,51 @@
+#include "ContactDetails.h"
+#include <iostream>
+
+ContactDetails::ContactDetails(std::optional<std::string> email, std::optional<std::string> phone, std::shared_ptr<Address> address)
+	: m_emailAddress{ email }, m_phoneNumber{ phone }, m_address{ address }
+{
+}
+
+std::optional<std::string> ContactDetails::getEmail() const
+{
+	if (m_emailAddress)
+		return m_emailAddress.value();
+	
+	return std::nullopt;
+}
+
+std::optional<std::string> ContactDetails::getPhoneNumber() const
+{
+	if (m_phoneNumber)
+		return m_phoneNumber.value();
+	
+	return std::nullopt;
+}
+
+void ContactDetails::setEmailAddres(const std::string& email)
+{
+	m_emailAddress = email; // TODO: use move-semantics instead??
+}
+
+void ContactDetails::setPhoneNumber(const std::string& phone)
+{
+	m_phoneNumber = phone;
+}
+
+void ContactDetails::setAddress(std::shared_ptr<Address> address)
+{
+	m_address = address;
+}
+
+
+//void ContactDetails::addAddress(std::shared_ptr<Address> address)
+//{
+//	m_address = address;
+//}
+
+
+
+void ContactDetails::printAddress() const
+{
+	std::cout << *m_address;
+}
---
+++ Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/ContactDetails.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Address.h"
+
+#include <memory>
+#include <optional>
+#include <string>
+
+
+class ContactDetails
+{
+	
+
+public:
+	ContactDetails(std::optional<std::string> email, std::optional<std::string> phone, std::shared_ptr<Address> address);
+	
+	// NEEDED?
+	std::optional<std::string> getEmail() const;
+	std::optional<std::string> getPhoneNumber() const;
+
+	//Address* getAddress() const; // return a shared pointer or just a raw pointer
+	
+	void setEmailAddres(const std::string& email);
+	void setPhoneNumber(const std::string& phone);
+	void setAddress(std::shared_ptr<Address> address); 
+
+	void printAddress() const; 
+
+private:
+	std::optional<std::string> m_emailAddress;
+	std::optional<std::string> m_phoneNumber;
+	std::shared_ptr<Address>   m_address;
+};
+
