9360228a63775242aa7fc094810bf7485f365c3d
Henrik Jangefelt <henke9105@hotmail.com>
2020-12-05 22:24:46 +0100

    Added init logic for setting up the hotel

--- Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/Hotel.cpp
+++ Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/Hotel.cpp
@@ -1,18 +1,83 @@
 #include "Hotel.h"
+#include "Address.h"
+#include "AmenityFactory.h"
+#include "RoomFactory.h"
 
-
-Hotel::Hotel(HotelDetails& details)
-	: m_details{ std::move(details) }
+Hotel::Hotel()
 {
+	initRooms();
+	initDetails();
+}
+
+Hotel::Hotel(HotelDetails& details, std::vector<Room> rooms)
+	: m_details{ std::move(details) }, m_rooms{ std::move(rooms) }
+{
+}
+
+void Hotel::initRooms()
+{
+	Room::RoomType type;
+	std::vector<std::shared_ptr<Amenity>> amenities;
+
+	for (int i{ 0 }; i <= 249; ++i)
+	{
+		if (i % 20 == 0) // Luxury (Vip)
+		{
+			type = Room::RoomType::Family;
+			amenities = AmenityFactory::getRoomAmenity(AmenityFactory::AmenityLevel::vip);
+		}
+		else if (i % 10 == 0) // Deluxe
+		{
+			type = i % 20 == 0 ? Room::RoomType::Double : Room::RoomType::Family;
+			amenities = AmenityFactory::getRoomAmenity(AmenityFactory::AmenityLevel::deluxe);
+		}
+		else // Standard
+		{
+			type = i % 2 == 0 ? Room::RoomType::Single : Room::RoomType::Double;
+			amenities = AmenityFactory::getRoomAmenity(AmenityFactory::AmenityLevel::standard);
+		}
+		m_rooms.push_back(*RoomFactory::getRoom(type, amenities));
+	}
+}
+
+void Hotel::initDetails()
+{
+	auto address{ std::make_shared<Address>() }; // Keep as shared_ptr??
+	address
+		->setCountry("United States")
+		.setCity("Colorado")
+		.setPostalCode("237")
+		.setStreetName("Rocky Road")
+		.setStreetNumber("37");
+		
+	HotelDetails details;
+	details
+		.setHotelName("The Overlook Hotel")
+		//.setDistanceToOcean(std::nullopt)
+		.setStarRating(3)
+		.setContactDetails({ "Overlook@hotel.com", "190 923 71 921", address }) // email, phone, address
+		.addAmenity({"Sauna", 0.0})
+		.addAmenity({"Labyrinth", 0.0})
+		.addAmenity({"Indoor-pool", 0.0 });
+
+	m_details = std::move(details);
+
 }
 
 
 void Hotel::addRoom(const Room& room)
 {
-	m_rooms.push_back(std::make_unique<Room>(room));
+	m_rooms.push_back(room);
+	//m_rooms.push_back(std::make_unique<Room>(room));
 }
 
 void Hotel::addBooking(const Booking& booking)
 {
 	m_bookings.push_back(std::move(booking));
+}
+
+std::ostream& operator<< (std::ostream& out, const Hotel& hotel)
+{
+	out << hotel.m_details;
+	return out;
 }
--- Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/Hotel.h
+++ Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/Hotel.h
@@ -5,16 +5,20 @@
 
 #include <memory>
 #include <vector>
-
 class Hotel
 {
 public:
-	using roomList_t = std::vector<std::unique_ptr<Room>>;
+	//using roomList_t = std::vector<std::unique_ptr<Room>>;
+	using roomList_t = std::vector<Room>;
 	using bookingList_t = std::vector<Booking>;
-	//using employeeList_t = std::vector<Employee>;
+	//using employeeList_t = std::vector<Employee>; -- use reference_wrapper?
 
 public:
-	Hotel(HotelDetails& details);
+	Hotel();
+	Hotel(HotelDetails& details, std::vector<Room> rooms);
+
+	void initRooms();
+	void initDetails();
 
 	void addRoom(const Room& room);
 	void addBooking(const Booking& booking);
@@ -24,6 +28,8 @@ public:
 	void sortBookings();
 	void showBookedDates(); // put in hotel?
 	double getPriceForBooking(); // PUT IN HOTEL?
+
+	friend std::ostream& operator<< (std::ostream& out, const Hotel& hotel);
 
 private:
 	HotelDetails   m_details;
