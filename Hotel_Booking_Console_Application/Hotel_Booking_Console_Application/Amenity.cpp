22cc6220f8316f71ad212a5049c83d5f12ab34e5
Henrik Jangefelt <henke9105@hotmail.com>
2020-12-05 22:34:22 +0100

    Added overloaded output operator

--- Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/Amenity.cpp
+++ Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/Amenity.cpp
@@ -7,9 +7,17 @@ Amenity::Amenity(const std::string& name, double surcharge)
 {
 }
 
+Amenity::Amenity(Amenity* amenity)
+	: m_featureName{ amenity->m_featureName }, m_surcharge{ amenity->m_surcharge }
+{
+	delete amenity;	// TODO: Check if correct way...
+	amenity = nullptr;
+}
+
 
 std::ostream& operator<< (std::ostream& out, const Amenity& amenity)
 {
-	out << "Amenity:\nfeature: " << amenity.m_featureName << "\nsurcharge: " << amenity.m_surcharge << '\n';
+	out << amenity.m_featureName;
+	//out << "Amenity:\nfeature: " << amenity.m_featureName << "\nsurcharge: " << amenity.m_surcharge << '\n';
 	return out;
 }
--- Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/Amenity.h
+++ Hotel_Booking_Console_Application/Hotel_Booking_Console_Application/Amenity.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <string>
-
+#include <iostream>
 class Amenity
 {
 public:
 	Amenity(const std::string& name, double surcharge);
-
+	Amenity(Amenity* amenity);
 	//void setFeatureName(const std::string& name);
+	void sayHi() { std::cout << "HI"; }
+
 	friend std::ostream& operator<< (std::ostream& out, const Amenity& amenity);
 
 private:
